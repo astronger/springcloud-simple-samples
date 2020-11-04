93bf221ff4602caf473094ba57b13cb795ea0394
limengtao <limt2530@163.com>
2020-11-04 11:37:48 +0800

    OpenFeign配置日志、数据压缩

--- openfeign/src/main/java/com/example/openfeign/OpenfeignApplication.java
+++ openfeign/src/main/java/com/example/openfeign/OpenfeignApplication.java
@@ -1,8 +1,10 @@
 package com.example.openfeign;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients  //开启feign支持
@@ -10,6 +12,11 @@ public class OpenfeignApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OpenfeignApplication.class, args);
+    }
+
+    @Bean
+    Logger.Level loggerLevel(){
+        return Logger.Level.FULL;
     }
 
 }
