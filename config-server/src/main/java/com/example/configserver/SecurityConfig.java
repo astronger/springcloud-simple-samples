c6f22112b64b22d8fffd7df40a3c0e05e06a4026
limengtao <limt2530@163.com>
2020-11-26 15:13:20 +0800

    Spring Cloud Bus 批量刷新&逐个刷新。

---
+++ config-server/src/main/java/com/example/configserver/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.configserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+}
